f60cb5f27697c160cb76e2ae517b5c9129de3ba1
Jason <JPriskomatis@gmail.com>
2024-08-27 17:18:40 +0300

    Fixd: Animations times

--- Assets/DustClean/Scripts/Indicator.cs
+++ Assets/DustClean/Scripts/Indicator.cs
@@ -42,7 +42,7 @@ namespace cleanDust
 
         IEnumerator Timer()
         {
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(0.5f);
 
             this.GetComponent<CanvasGroup>().DOFade(1f, 2f);
             textIndicator.GetComponent<CanvasGroup>().DOFade(1f, 2f);
@@ -53,7 +53,7 @@ namespace cleanDust
 
         IEnumerator StartAnimation()
         {
-            yield return new WaitForSeconds(3f);
+            yield return new WaitForSeconds(2f);
 
             // Move left
             uiElement.DOAnchorPosX(originalPos.x - moveDistance, moveDuration).SetEase(Ease.Linear).OnComplete(() =>
@@ -75,7 +75,7 @@ namespace cleanDust
 
         private IEnumerator FinishAnimations()
         {
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(1f);
             textIndicator.GetComponent<CanvasGroup>().DOFade(0f, 1f);
 
             this.GetComponent<CanvasGroup>().DOFade(0f, 1f).OnComplete(() =>
